5dba78327ec600132c10d1375aaf84ae71bd3631
Emmanuel Ibuoye <ibuoyeemmanuel8@gmail.com>
2025-12-10 10:38:46 +0100

    add motivation app

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import requests
+
+app = FastAPI()
+
+base_url = "https://zenquotes.io/api/random/"
+
+templates = Jinja2Templates(directory="templates")
+
+@app.post("/")
+def get_quote(request: Request):
+    response = requests.get(base_url)
+    data = response.json()
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
